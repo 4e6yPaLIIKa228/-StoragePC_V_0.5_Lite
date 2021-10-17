2a6712c8f615556a68bab2bc45ad38cd2cf858df
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2021-10-17 17:25:16 +0300

    test
    test

---
+++ AddTechnic.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using YchetPer.Connection;
+using System.Data;
+
+
+namespace YchetPer
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTechnic.xaml
+    /// </summary>
+    public partial class AddTechnic : Window
+    {
+        public AddTechnic()
+        {
+            InitializeComponent();
+            CbFill();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        public void CbFill()  //Данные для комбобоксов 
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+            {
+                try
+                {
+                    connection.Open();
+                    string query1 = $@"SELECT * FROM Types"; // Типы
+                    string query2 = $@"SELECT * FROM Conditions"; // Состояние
+                    string query3 = $@"SELECT * FROM NumberKabs"; // Кабинеты
+                    string query4 = $@"SELECT  Devices.Title FROM Devices"; // Названия
+                    //----------------------------------------------
+                    SQLiteCommand cmd1 = new SQLiteCommand(query1, connection);
+                    SQLiteCommand cmd2 = new SQLiteCommand(query2, connection);
+                    SQLiteCommand cmd3 = new SQLiteCommand(query3, connection);
+                    SQLiteCommand cmd4 = new SQLiteCommand(query4, connection);
+                    //----------------------------------------------
+                    SQLiteDataAdapter SDA1 = new SQLiteDataAdapter(cmd1);
+                    SQLiteDataAdapter SDA2 = new SQLiteDataAdapter(cmd2);
+                    SQLiteDataAdapter SDA3 = new SQLiteDataAdapter(cmd3);
+                    SQLiteDataAdapter SDA4 = new SQLiteDataAdapter(cmd4);
+                    //----------------------------------------------
+                    DataTable dt1 = new DataTable("Types");
+                    DataTable dt2 = new DataTable("Conditions");
+                    DataTable dt3 = new DataTable("NumberKabs");
+                    DataTable dt4 = new DataTable("Devices");
+                    //----------------------------------------------
+                    SDA1.Fill(dt1);
+                    SDA2.Fill(dt2);
+                    SDA3.Fill(dt3);
+                    SDA4.Fill(dt4);
+                    //----------------------------------------------
+                    CbClass.ItemsSource = dt1.DefaultView;
+                    CbClass.DisplayMemberPath = "Class";
+                    CbClass.SelectedValuePath = "ID";
+                    //----------------------------------------------
+                    CbCondition.ItemsSource = dt2.DefaultView;
+                    CbCondition.DisplayMemberPath = "Condition";
+                    CbCondition.SelectedValuePath = "ID";
+                    //----------------------------------------------
+                    CbNumKabn.ItemsSource = dt3.DefaultView;
+                    CbNumKabn.DisplayMemberPath = "NumKab";
+                    CbNumKabn.SelectedValuePath = "ID";
+                    ////----------------------------------------------
+                    CbTitle.ItemsSource = dt4.DefaultView;
+                    CbTitle.DisplayMemberPath = "Title";
+                    CbTitle.SelectedValuePath = "ID";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
+
+
+            private void BtnAdd_Click(object sender, RoutedEventArgs e) //Добавление
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+            {
+                connection.Open();
+                string query = $@"INSERT INTO Devices(IDType,IDKabuneta,Title,Number,IDCondition) values (@IDType,@IDKabuneta,@Title,@Number,@IDCondition);";
+                SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                try
+                {
+                    //cmd.Parameters.AddWithValue("@SN", TbSN.Text);
+                    //cmd.Parameters.AddWithValue("@FN", TbFN.Text);
+                    //cmd.Parameters.AddWithValue("@MN", TbMN.Text);
+                    //cmd.Parameters.AddWithValue("@Date", DpB.Text);
+                    //cmd.Parameters.AddWithValue("@Phone", TbPhone.Text);
+                    //cmd.Parameters.AddWithValue("@Post", );
+                    //cmd.Parameters.AddWithValue("@Stat", );
+                    //cmd.ExecuteNonQuery();
+                    this.Close();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using YchetPer.Connection;
+using System.Data;
+
+
+namespace YchetPer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayData();
+            DGAllEmp.Columns[0].IsReadOnly = true;
+            DGAllEmp.Columns[1].IsReadOnly = true;
+            DGAllEmp.Columns[2].IsReadOnly = true;
+            DGAllEmp.Columns[3].IsReadOnly = true;
+            DGAllEmp.Columns[4].IsReadOnly = true;
+            DGAllEmp.Columns[5].IsReadOnly = true;
+            DGAllEmp.Columns[6].IsReadOnly = true;
+            //DGAllEmp.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            //DGAllEmp.AllowUserToAddRows = false;
+
+
+        }
+
+        public void DisplayData()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = $@"SELECT Devices.ID, Types.Class, Devices.Title, Devices.Number, Conditions.Condition ,NumberKabs.NumKab ,Devices.StartWork 
+                                      FROM Devices JOIN  Types
+                                      ON Devices.IDType = Types.ID
+                                      JOIN  Conditions
+                                      ON Devices.IDCondition = Conditions.ID
+                                      JOIN  NumberKabs
+                                      ON Devices.IDKabuneta = NumberKabs.ID;";
+                    SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                    DataTable DT = new DataTable("Devices");
+                    SQLiteDataAdapter SDA = new SQLiteDataAdapter(cmd);
+                    SDA.Fill(DT);
+                    DGAllEmp.ItemsSource = DT.DefaultView;
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+
+
+            }
+        }
+        public void UpdateDG()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = $@"SELECT Devices.ID, Types.Class, Devices.Title, Devices.Number, Conditions.Condition ,NumberKabs.NumKab ,Devices.StartWork 
+                                    FROM Devices JOIN  Types
+                                    ON Devices.IDType = Types.ID
+                                    JOIN  Conditions
+                                    ON Devices.IDCondition = Conditions.ID
+                                    JOIN  NumberKabs
+                                    ON Devices.IDKabuneta = NumberKabs.ID;";
+                    SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                    DataTable DT = new DataTable("Devices");
+                    SQLiteDataAdapter SDA = new SQLiteDataAdapter(cmd);
+                    SDA.Fill(DT);
+                    DGAllEmp.ItemsSource = DT.DefaultView;
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+
+
+            }
+        }
+        public void Delete()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+            {
+                try
+                {
+
+                    foreach (var item in DGAllEmp.SelectedItems.Cast<DataRowView>())
+                    {
+                        string query1 = $@"DELETE FROM Devices WHERE id = " + item["ID"];
+                        connection.Open();
+
+                        SQLiteCommand cmd1 = new SQLiteCommand(query1, connection);
+                        DataTable DT = new DataTable("Devices");
+                        cmd1.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+
+
+            }
+        }
+        private void BtnUpd_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDG();
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            Delete();
+            UpdateDG();
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddTechnic AddTec = new AddTechnic();
+            AddTec.Owner = this;
+            AddTec.ShowDialog();
+
+
+        }
+
+
+    }
+}
+
